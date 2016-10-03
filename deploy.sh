2ed5dffe36d84a30fff687d3b0daf4875c855e2c
Todd Iverson <tiverson@smumn.edu>
2016-10-03 13:40:08 -0500

    added force add for the build folder to deploy.sh and fixed a image

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
 touch ./build/stat110/.nojekyll
+git add build/ -f
 git add ./build/stat110/.nojekyll -f
-git commit -m "Added the .nojekyll fill as part of deployment"
+git commit -m "Added the current build and .nojekyll file as part of deployment"
 git subtree split --prefix build/stat110 -b gh-pages
 git push -f origin gh-pages:gh-pages
 git branch -D gh-pages
